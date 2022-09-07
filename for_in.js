39ce85b64892fe7eb7625918057f411fcc34c17c
kwooyoung <111227567+kwooyoung@users.noreply.github.com>
2022-09-07 22:11:46 +0900

    for_in, scope, shadowing의 이해 학습
    programmers

---
+++ for_in.js
@@ -0,0 +1,26 @@
+var obj ={
+  name:"object",
+  weight:30,
+  isObject:true,
+  arr:[1,2,3],
+  obj:{property:1}
+};
+
+
+/*
+console.log("for 구문으로 object property 반복하기");
+
+var property_list = Object.keys(obj);
+
+for( var i=0 ; i<property_list.length ; i++ ){
+    var propertyName = property_list[i];
+    console.log( "\t", propertyName, ": ", obj[propertyName] );
+}
+
+
+*/
+console.log("\n\nFor in 구문으로 object poperty 반복하기");
+
+for( var propertyName in obj ){
+    console.log( "\t", propertyName, ": ", obj[propertyName] );
+}
---
+++ scope.js
@@ -0,0 +1,48 @@
+//scope 변수, 선언된 변수가 어느 위치에서 유효한가?
+
+function a(){
+  var v_a= "a";
+
+  function b(){
+    var v_b="b";
+    console.log("b : ",typeof(v), typeof(v_a),typeof(v_b));
+    
+  }
+
+  b();
+  console.log("a : ",typeof(v), typeof(v_a),typeof(v_b));
+}
+
+
+var v="v";
+a();
+console.log("ㅇ : ",typeof(v), typeof(v_a),typeof(v_b));
+/*
+
+
+a{
+  함수 v_a 선언
+  b{
+    함수 v_b선언
+      b의 type of 탐색
+
+   }
+   b 호출
+   a의 type of 탐색
+
+}
+  a 함수 호출
+  v의 type of 탐색
+
+
+  출력 결과
+  b :  string string string
+  a :  string string undefined
+  ㅇ :  string undefined undefined
+
+변수의 범위(scope)는 function의 범위(scope)를 따른다.
+객체(변수)는 선언된 함수 안에서만 접근이 가능
+v는 전역 스코프라 어느곳에서나 접근이 가능하다.
+반대로 v_a, v_b 함수는 지역변수로 그 안에서만 접근이 가능
+
+*/
